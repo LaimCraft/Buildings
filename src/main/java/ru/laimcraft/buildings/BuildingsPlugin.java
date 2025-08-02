d890657c0559564e16a0c11fd92157604b514f87
isaevisa05 <isaevisao55@gmail.com>
2025-08-02 15:00:00 +0300

    Create

---
+++ src/main/java/ru/laimcraft/buildings/BuildingsPlugin.java
@@ -0,0 +1,14 @@
+package ru.laimcraft.buildings;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BuildingsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {}
+}
